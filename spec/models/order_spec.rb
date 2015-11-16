4e87e301ec08d2b9f47dc68d6e42565a178ec2b6
Andrew Platkin <andrew.platkin@gmail.com>
2015-11-15 23:09:01 -0600

    fixed up formatting of tests for order spec, added spec for showing

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,103 +1,101 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  describe 'validation' do
-    context 'name' do
-      it 'will prevent creation of order if name has 0 characters' do
-        order = build(:order, :with_all_needed_associations)
-        order.name = nil
-        expect(order.save).to eq false
-      end
-      it 'allows creation of order if name has atleast 1 character' do
-        order = build(:order, :with_all_needed_associations)
-        order.name = "Andrew"
-        expect(order.save).to eq true
-      end
+  describe '#name' do
+    it 'will prevent creation of order if name has 0 characters' do
+      order = build(:order, :with_all_needed_associations)
+      order.name = nil
+      expect(order.save).to eq false
     end
-
-    context 'email' do
-      it 'will prevent creation of order if email has 0 characters' do
-        order = build(:order, :with_all_needed_associations)
-        order.email = nil
-        expect(order.save).to eq false
-      end
-      it 'will prevent creation of order if email is invalid' do
-        order = build(:order, :with_all_needed_associations)
-        order.email = "andrew@test"
-        expect(order.save).to eq false
-      end
-      it 'allows creation of order if email is valid' do
-        order = build(:order, :with_all_needed_associations)
-        order.email = "aplatkin@hotmail.com"
-        expect(order.save).to eq true
-      end
+    it 'allows creation of order if name has atleast 1 character' do
+      order = build(:order, :with_all_needed_associations)
+      order.name = "Andrew"
+      expect(order.save).to eq true
     end
+  end
 
-    context 'creditcardnum' do
-      it 'will prevent creation of order if credit card is not atleast 14 characters' do
-        order = build(:order, :with_all_needed_associations)
-        order.creditcardnum = "12312312"
-        expect(order.save).to eq false
-      end
-      it 'will prevent creation of order if credit card is greater than 16 characters' do
-        order = build(:order, :with_all_needed_associations)
-        order.creditcardnum = "1123123123123123123123123123123123"
-        expect(order.save).to eq false
-      end
-      it 'allows creation of order if credit card is 16 characters' do
-        order = build(:order, :with_all_needed_associations)
-        order.creditcardnum = "1234123412341234"
-        expect(order.save).to eq true
-      end
+  describe '#email' do
+    it 'will prevent creation of order if email has 0 characters' do
+      order = build(:order, :with_all_needed_associations)
+      order.email = nil
+      expect(order.save).to eq false
     end
-
-    context 'expirationdate' do
-      it 'will prevent creation of order if expiration date is not provided' do
-        order = build(:order, :with_all_needed_associations)
-        order.expirationdate = nil
-        expect(order.save).to eq false
-      end
-      it 'allows creation of order if expiration date is provided' do
-        order = build(:order, :with_all_needed_associations)
-        order.expirationdate = "12-2015"
-        expect(order.save).to eq true
-      end
+    it 'will prevent creation of order if email is invalid' do
+      order = build(:order, :with_all_needed_associations)
+      order.email = "andrew@test"
+      expect(order.save).to eq false
     end
-
-    context 'quantity' do
-      it 'will prevent creation of order if quantity is not not greater than 0' do
-        order = build(:order, :with_all_needed_associations)
-        order.quantity = 0
-        expect(order.save).to eq false
-      end
-      it 'will prevent creation of order if quantity is not an integer' do
-        order = build(:order, :with_all_needed_associations)
-        order.quantity = 1.55
-        expect(order.save).to eq false
-      end
-      it 'will prevent creation of order if quantity requested is more than quantity of tickets available' do
-        order = build(:order, :with_all_needed_associations)
-        order.quantity = (order.showing.tickets_remaining + 1)
-        expect(order.save).to eq false
-      end
-      it 'allows creation of order if quantity is provided and tickets are available' do
-        order = build(:order, :with_all_needed_associations)
-        order.quantity = 1
-        expect(order.save).to eq true
-      end
+    it 'allows creation of order if email is valid' do
+      order = build(:order, :with_all_needed_associations)
+      order.email = "aplatkin@hotmail.com"
+      expect(order.save).to eq true
     end
+  end
 
-    context 'after movie start' do
-      it 'will prevent creation of order if tickets are trying to be bought and the movie date has already passed' do
-        order = build(:order, :with_all_needed_associations)
-        order.showing.showtime = 10.days.ago
-        expect(order.save).to eq false
-      end
-      it 'allows creation of order if the movie showtime is upcoming' do
-        order = build(:order, :with_all_needed_associations)
-        order.showing.showtime = 5.days.from_now
-        expect(order.save).to eq true
-      end
+  describe '#creditcardnum' do
+    it 'will prevent creation of order if credit card is not atleast 14 characters' do
+      order = build(:order, :with_all_needed_associations)
+      order.creditcardnum = "12312312"
+      expect(order.save).to eq false
+    end
+    it 'will prevent creation of order if credit card is greater than 16 characters' do
+      order = build(:order, :with_all_needed_associations)
+      order.creditcardnum = "1123123123123123123123123123123123"
+      expect(order.save).to eq false
+    end
+    it 'allows creation of order if credit card is 16 characters' do
+      order = build(:order, :with_all_needed_associations)
+      order.creditcardnum = "1234123412341234"
+      expect(order.save).to eq true
+    end
+  end
+
+  describe '#expirationdate' do
+    it 'will prevent creation of order if expiration date is not provided' do
+      order = build(:order, :with_all_needed_associations)
+      order.expirationdate = nil
+      expect(order.save).to eq false
+    end
+    it 'allows creation of order if expiration date is provided' do
+      order = build(:order, :with_all_needed_associations)
+      order.expirationdate = "12-2015"
+      expect(order.save).to eq true
+    end
+  end
+
+  describe '#quantity' do
+    it 'will prevent creation of order if quantity is not not greater than 0' do
+      order = build(:order, :with_all_needed_associations)
+      order.quantity = 0
+      expect(order.save).to eq false
+    end
+    it 'will prevent creation of order if quantity is not an integer' do
+      order = build(:order, :with_all_needed_associations)
+      order.quantity = 1.55
+      expect(order.save).to eq false
+    end
+    it 'will prevent creation of order if quantity requested is more than quantity of tickets available' do
+      order = build(:order, :with_all_needed_associations)
+      order.quantity = (order.showing.tickets_remaining + 1)
+      expect(order.save).to eq false
+    end
+    it 'allows creation of order if quantity is provided and tickets are available' do
+      order = build(:order, :with_all_needed_associations)
+      order.quantity = 1
+      expect(order.save).to eq true
+    end
+  end
+
+  describe '#created_at' do
+    it 'will prevent creation of order if tickets are trying to be bought and the movie date has already passed' do
+      order = build(:order, :with_all_needed_associations)
+      order.showing.showtime = 5.hours.ago
+      expect(order.save).to eq false
+    end
+    it 'allows creation of order if the movie showtime is upcoming' do
+      order = build(:order, :with_all_needed_associations)
+      order.showing.showtime = 5.days.from_now
+      expect(order.save).to eq true
     end
   end
 end
--- spec/models/showing_spec.rb
+++ spec/models/showing_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Showing, type: :model do
-  
+  describe '#tickets_available' do
+    it 'returns false if the remaining tickets for the showtime is 0' do
+      order = create(:order, :with_all_needed_associations)
+      order.showing.auditorium.capacity = 1
+      expect(order.showing.tickets_available).to eq false
+    end
+    it 'returns false if the showtime is in the past' do
+      order = create(:order, :with_all_needed_associations)
+      order.showing.showtime = 5.hours.ago
+      expect(order.showing.tickets_available).to eq false
+    end
+    it 'returns true if the remaining tickets are available and the showtime is in the future' do
+      showing = create(:showing)
+      showing.auditorium.capacity = 10
+      expect(showing.tickets_available).to eq true
+    end
+  end
+
+  describe '#tickets_remaining' do
+    it 'returns the difference of between the summation of all quantities of orders associated with the showtime and the auditoriums capacity' do
+      showing = create(:showing)
+      order_one = create(:order, showing: showing)
+      order_two = create(:order, showing: showing)
+      remaining_count = showing.auditorium.capacity - (order_one.quantity + order_two.quantity)
+      expect(showing.tickets_remaining).to eq remaining_count
+    end
+  end
 end
