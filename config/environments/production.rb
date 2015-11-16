15c90fc345f941f3467a5fdc94303fba034f73cc
Andrew Platkin <andrew.platkin@gmail.com>
2015-11-15 19:15:08 -0600

    attempt at fixing emails in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.raise_delivery_errors = true
+  
+  config.action_mailer.default_url_options = { :host => ENV["smtp_domain"] }
+
   config.action_mailer.delivery_method = :smtp
 
   # SMTP settings for email
