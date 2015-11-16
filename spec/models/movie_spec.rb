d5db1bda3cd2c161f927c0edd7b4faee955e4b9f
Andrew Platkin <andrew.platkin@gmail.com>
2015-11-15 23:16:19 -0600

    added a spec to test out filtered movies / showings depending on date passed

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-
+  describe '.with_showings_and_ratings' do
+    it 'returns all showings that fall within the day passed' do
+      showing_one = create(:showing, showtime: Date.today)
+      showing_two = create(:showing, showtime: Date.today)
+      showing_three = create(:showing, showtime: Date.tomorrow)
+      showing_four = create(:showing, showtime: Date.yesterday)
+      expect(described_class.with_showings_and_ratings(Date.today).count).to eq 2
+    end
+  end
 end
